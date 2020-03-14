f08562f3b133d336578dc5cee97423f163aabf4d
pom <pomp270@gmail.com>
2020-03-14 15:50:53 +0700

    เพิ่มหน้าหมวดหมู่ รายการ

---
+++ V1/home.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include "link.php" ?>
+
+    <?php require_once('query.php'); ?>
+    <?php
+
+
+
+    ?>
+
+    <title>ร้านเช่าอุปกรณ์ตกแต่ง</title>
+</head>
+
+<body id="page-top">
+    <div id="wrapper">
+
+        <!-- อันนี้ไว้เรียกใช้แท็บข้างๆๆ -->
+        <?php include "layout_user.php" ?>
+
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+                <!-- อันนี้ไว้เรียกใช้แท็บบน -->
+                <?php include "Topbar.php" ?>
+                <!-- End of Topbar -->
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+
+
+
+                    <!-- เริ่ม -->
+                    <div>
+
+                        <div class="row">
+                            <div class="col-xl-12 col-8 mb-4">
+                                <div class="card">
+                                    <form class="user" action="Profile.php" method="post">
+                                        <div class="card-body">
+                                            <div class="row mb-4">
+                                                <div class="col-12">
+                                                    <span class="link-active " style="font-size: 50px; color:black;">WELCOME</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <!-- จบ -->
+
+                </div>
+                <!-- End of Main Content -->
+
+
+            </div>
+            <!-- End of Content Wrapper -->
+
+        </div>
+        <!-- End of Page Wrapper -->
+
+        <!-- Scroll to Top Button-->
+        <a class="scroll-to-top rounded" href="#page-top">
+            <i class="fas fa-angle-up"></i>
+        </a>
+</body>
+<?php include_once("modal.php") ?>
+
+</html>
+
+<script>
+    function editfuser() {
+        $("#modaladdsub").modal('show');
+    }
+</script>
---
+++ V1/stock.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include "link.php" ?>
+    <title>Sweet Home</title>
+</head>
+
+<body id="page-top">
+    <div id="wrapper">
+
+        <!-- อันนี้ไว้เรียกใช้แท็บข้างๆๆ -->
+        <?php include "layout_user.php" ?>
+        <?php include "helper_func.inc.php" ?>
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+                <!-- อันนี้ไว้เรียกใช้แท็บบน -->
+                <?php include "Topbar.php" ?>
+                <!-- End of Topbar -->
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+
+                    <!-- Page Heading -->
+                    <div bgcolor=“green” class="d-sm-flex align-items-center justify-content-between mb-4">
+
+                    </div>
+
+                    <div class="row">
+
+                        <div class="col-xl-3 col-12 mb-4">
+
+                            <div class="card border-left-primary card-color-info shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="font-weight-bold  text-uppercase mb-1">รายการสินค้า</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800">5000 ชิ้น</div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fas fa-home fa-2x"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-xl-3 col-12 mb-4">
+                            <div class="card border-left-primary card-color-info shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="font-weight-bold  text-uppercase mb-1">เพิ่มสินค้า</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800">+1 รายการ</div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="far fa-plus-square fa-2x"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Content Row -->
+                    <div class="card shadow mb-4">
+                        <div class="card-header py-3">
+                            <h6 class="m-0 font-weight-bold text-primary">สินค้าทั้งหมด</h6>
+
+                        </div>
+
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <div id="dataTable_wrapper" class="dataTables_wrapper dt-bootstrap4">
+                                    <div class="row center">
+                                        <div class="col-sm-12">
+                                            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+
+                                                <thead>
+                                                    <tr>
+                                                        <th>ลำดับ</th>
+                                                        <td>รูปสินค้า</td>
+                                                        <th>หมวดหมู่</th>
+                                                        <th>รายการ</th>
+                                                        <th>จำนวน</th>
+                                                        <th>ราคาชิ้นละ(บาท)</th>
+                                                        <th>จัดการ</th>
+                                                    </tr>
+                                                </thead>
+                                                <tfoot>
+                                                    <tr>
+                                                        <th>ลำดับ</th>
+                                                        <td>รูปสินค้า</td>
+                                                        <th>หมวดหมู่</th>
+                                                        <th>รายการ</th>
+                                                        <th>จำนวน</th>
+                                                        <th>ราคาชิ้นละ(บาท)</th>
+                                                        <th>จัดการ</th>
+                                                    </tr>
+                                                </tfoot>
+                                                <tbody>
+
+                                                    <tr style="text-align:center;">
+                                                        <td>1</td>
+
+                                                        <td>ดอกไม้</td>
+                                                        <td>ดอกมะลิ</td>
+                                                        <td>50</td>
+                                                        <td>5</td>
+                                                        <td>
+                                                            <button type="button" id="btn_edit" class="btn btn-warning btn-sm tt " title='แก้ไขหมวดหมู่'>
+                                                                <i class="fas fa-edit"></i>
+                                                            </button>
+                                                            <button type="button" id="btn_alt" class="btn btn-danger btn-sm tt " title='ลบหมวดหมู่'>
+                                                                <i class="far fa-trash-alt"></i>
+                                                            </button>
+
+                                                        </td>
+                                                    </tr>
+                                                    <tr style="text-align:center;">
+                                                        <td>2</td>
+                                                        <td>ธงลายการ์ตูน</td>
+                                                        <td>ธงลายเจ้าหญิง</td>
+                                                        <td>20</td>
+                                                        <td>15</td>
+                                                        <td>
+                                                            <button type="button" id="btn_edit" class="btn btn-warning btn-sm tt " title='แก้ไขหมวดหมู่'>
+                                                                <i class="fas fa-edit"></i>
+                                                            </button>
+                                                            <button type="button" id="btn_alt" class="btn btn-danger btn-sm tt " title='ลบหมวดหมู่'>
+                                                                <i class="far fa-trash-alt"></i>
+                                                            </button>
+
+                                                        </td>
+                                                    </tr>
+
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- /.container-fluid -->
+
+            </div>
+            <!-- End of Main Content -->
+
+
+        </div>
+        <!-- End of Content Wrapper -->
+
+    </div>
+    <!-- End of Page Wrapper -->
+
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
+
+
+
+</body>
+
+</html>
